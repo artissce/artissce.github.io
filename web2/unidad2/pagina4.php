45cc10bc5c84934e460ce8700cdc1ddf078311a0
artissce <artis.2004.07@gmail.com>
2022-09-20 17:55:51 -0500

    unidad2

---
+++ web2/unidad2/pagina4.php
@@ -0,0 +1,15 @@
+<html>
+	<head><title>Saludo con PHP</title></head>
+<body>
+<?php
+	$nombre="Luis";
+	$edad	=18;
+	echo "Hola <b>" .  $nombre . "</b> Que tengas bonito día";
+	echo " Tienes <b>" . $edad . "</b> años ";
+?>
+</body>
+<footer> 
+    <hr>
+    <p style="text-align:center;">Ana Karen Cuenca Esquivel - 177932<br>UNIVERSIDAD POLITECNICA DE SAN LUIS POTOSI<br>Programacion Web I</p>
+</footer>
+</html>
---
+++ web2/unidad2/saludo3.php
@@ -0,0 +1,25 @@
+<?php
+/*strtoupper($palabra); // cambia a mayúscula
+sort($palabra); //ordena ASC los caracteres
+$pal1str_split(“hola");//”h”,”o”,”l”,”a”
+implode('', $pal1);//”hola”
+*/
+$palabra1=$_POST['palabra1'];
+$palabra2=$_POST['palabra2'];
+strtoupper($palabra1);
+strtoupper($palabra2);
+$pal1 = str_split($palabra1);//”h”,”o”,”l”,”a”
+$pal2 = str_split($palabra2);//”h”,”o”,”l”,”a”
+sort($pal1);
+sort($pal2);
+
+if ($pal1 == $pal2)
+{   
+    echo "$pal1 y  $pal2 son anagramas";
+}
+else
+{
+    echo $pal1. "y". $pal2. " no son anagramas";
+}
+
+?>
